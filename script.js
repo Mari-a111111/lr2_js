240cdc62d8066aa92833fdd56bd536ca8b02aeb0
Mari-a111111 <ndchfjfvm@gmail.com>
2025-11-01 15:29:01 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-// 1) Запит імені та виведення привітання через alert
 let userName = prompt("Введіть ваше ім'я:");
 alert(`Hello, ${userName}! Welcome to JavaScript`);
 
-// 2) Запит віку та розрахунок року народження
 let age = prompt("Введіть ваш вік:");
 let currentYear = new Date().getFullYear();
 let birthYear = currentYear - age;
 alert(`You were born in ${birthYear}`);
 
-// 3) Запит імені та прізвища, виведення повного імені двома способами
 let firstName = prompt("Введіть ваше ім'я:");
 let lastName = prompt("Введіть ваше прізвище:");
 
-// Через конкатенацію (+)
 console.log("Your full name is " + firstName + " " + lastName);
 
-// Через шаблонний рядок
 console.log(`Your full name is ${firstName} ${lastName}`);
